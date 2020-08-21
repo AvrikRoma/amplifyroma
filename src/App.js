d423114f23a20b43910557e1279bfea81bb74094
AvrikRoma <69822978+AvrikRoma@users.noreply.github.com>
2020-08-21 12:50:18 -0500

    added auth

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" /> <h1> Te amo Chris </h1>
+        <img src={logo} className="App-logo" alt="logo" /> <h1> Te amo CJ </h1>
       </header>
     </div>
   );
